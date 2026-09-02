b1c59070f0f069e971025e9ce451e53f1cb8ae6d
Bohdan Pukhno <pukhnobg@gmail.com>
2026-09-02 16:52:20 +0300

    feat: add register contract

---
+++ contracts/source-chain/Register.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {IEnergyCreditLedger} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+abstract contract Register is AccessControl {
+    bytes32 public constant REGISTER_ROLE = keccak256("REGISTER_ROLE");
+    bytes32 public constant ORACLE_ROLE = keccak256("ORACLE_ROLE");
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function grantRegisterRole(address register) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(REGISTER_ROLE, register);
+    }
+
+    function addOracle(address oracle) external onlyRole(REGISTER_ROLE) {
+        _grantRole(ORACLE_ROLE, oracle);
+    }
+}
